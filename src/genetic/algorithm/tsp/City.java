bf00f77efbf04e63e09217506db34b6e5a397810
root <schmittjoaopedro@gmail.com>
2017-08-04 21:30:34 -0300

    add tsp code

---
+++ src/genetic/algorithm/tsp/City.java
@@ -0,0 +1,40 @@
+package genetic.algorithm.tsp;
+
+public class City {
+
+    private int x;
+    private int y;
+
+    // Constructs a randomly placed city
+    public City() {
+        this.x = (int) (Math.random() * 200);
+        this.y = (int) (Math.random() * 200);
+    }
+
+    // Constructs a city at chosen x, y location
+    public City(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Gets city's x coordinate
+    public int getX() {
+        return this.x;
+    }
+
+    // Gets city's y coordinate
+    public int getY() {
+        return this.y;
+    }
+
+    // Gets the distance to given city
+    public double distanceTo(City city) {
+        return Math.sqrt(Math.pow(city.getX() - this.getX(), 2.0) + Math.pow(city.getY() - this.getY(), 2.0));
+    }
+
+    @Override
+    public String toString() {
+        return getX() + ", " + getY();
+    }
+
+}
---
+++ src/genetic/algorithm/tsp/PlotRoute.java
@@ -0,0 +1,85 @@
+package genetic.algorithm.tsp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PlotRoute extends JPanel {
+
+    private Tour tour;
+    private List<Object> shapes = new ArrayList<>();
+
+    public PlotRoute() {
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(220, 220));
+    }
+
+    public void drawTour(Tour tour) {
+        this.tour = tour;
+        shapes.clear();
+        for(int i = 0; i < tour.tourSize(); i++) {
+            shapes.add(new Circle(tour.getCity(i).getX(), tour.getCity(i).getY()));
+        }
+        for (int i = 0; i < tour.tourSize(); i++) {
+            City from = tour.getCity(i);
+            City dest;
+            if (i + 1 < tour.tourSize()) {
+                dest = tour.getCity(i + 1);
+            } else {
+                dest = tour.getCity(0);
+            }
+            shapes.add(new Line(from.getX(), from.getY(), dest.getX(), dest.getY()));
+        }
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        for (Object s : shapes) {
+            if (s instanceof Circle) {
+                ((Circle) s).draw(graphics);
+            } else if (s instanceof Line) {
+                ((Line) s).draw(graphics);
+            }
+        }
+    }
+
+    class Circle {
+        int x, y, width, height;
+        public Circle(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public void draw(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            Ellipse2D.Double circle = new Ellipse2D.Double(x, y, 10, 10);
+            g2d.setColor(Color.GRAY);
+            g2d.fill(circle);
+        }
+    }
+
+    class Line {
+
+        int x1, y1, x2, y2;
+
+        public Line(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public void draw(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            Line2D line = new Line2D.Float(this.x1, this.y1, this.x2, this.y2);
+            g2d.draw(line);
+        }
+
+    }
+}
