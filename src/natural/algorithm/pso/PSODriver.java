58b77d3249902358460b512d4cb6ba4845c2a3d8
root <schmittjoaopedro@gmail.com>
2017-08-29 21:09:52 -0300

    add reference

--- src/natural/algorithm/pso/PSODriver.java
+++ src/natural/algorithm/pso/PSODriver.java
@@ -2,6 +2,8 @@ package natural.algorithm.pso;
 
 /**
  * Created by root on 29/08/17.
+ *
+ * https://github.com/therealmanalu/pso-example-java
  */
 public class PSODriver {
     public static void main(String args[]) {
